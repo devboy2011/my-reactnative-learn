a5686fc0d341e672766070f436dddfb8878d11d6
devboy2011 <buituananhit.contact@gmail.com>
2025-10-17 04:55:31 +0700

    clear redundant

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import { registerRootComponent } from 'expo';
 
-import App from './bt_props/App';
+import App from './baitap6/App';
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
